af5c5a9177a0ac137aaa415f3d27084caa1a47df
bokerface <waskitodamar17@gmail.com>
2025-09-23 08:31:50 +0700

    create monster class

---
+++ src/main/java/com/itoo/Monster.java
@@ -0,0 +1,9 @@
+package com.itoo;
+
+public class Monster extends Character {
+
+    public Monster(String name, int health, int attackPower, int defense) {
+        super(name, health, attackPower, defense);
+    }
+
+}
